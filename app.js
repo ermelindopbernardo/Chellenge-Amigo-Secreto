b887de00776b7f2b5f5a68feb085ac975ac8e44c
ermelindopbernardo <bernardotooroffice@gmail.com>
2025-02-21 00:04:19 +0100

    create as funcoes addAmigo e Limpar versao basica

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeAmigos = [];
+
+function limparCampo() {
+    let addAmigo = document.querySelector('input');
+    addAmigo.value = '';
+}
+
+function adicionarAmigo() {
+    let addAmigo = document.querySelector('input').value;
+    if (addAmigo == '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+
+    listaDeAmigos.push(addAmigo);
+    limparCampo();
+    console.log(listaDeAmigos);
+}
+
