22f289d00cf360df1556d9f8e0bf8bd40346e8ac
ermelindopbernardo <bernardotooroffice@gmail.com>
2025-02-23 00:22:16 +0100

    criancao das funcoes addAmigo e Actualizar Lista para aparecer a lista na tela

--- app.js
+++ app.js
@@ -2,20 +2,25 @@
 
 let listaDeAmigos = [];
 
-function limparCampo() {
-    let addAmigo = document.querySelector('input');
-    addAmigo.value = '';
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+    if (nome == "") {
+       alert("Por favor, insira um nome.");
+       return;
+    }
+    listaDeAmigos.push(nome);
+    atualizarLista();
+    input.value = "";
 }
 
-function adicionarAmigo() {
-    let addAmigo = document.querySelector('input').value;
-    if (addAmigo == '') {
-        alert('Por favor, insira um nome.');
-        return;
-    }
-
-    listaDeAmigos.push(addAmigo);
-    limparCampo();
-    console.log(listaDeAmigos);
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    listaDeAmigos.forEach(nome => {
+        let li = document.createElement("li");
+        li.textContent = nome;
+        lista.appendChild(li);
+    });
 }
 
